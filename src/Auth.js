edd03f2a9493517bbc5dd4fe1cc6c35c32a4eb5e
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2020-09-15 22:49:48 +0200

    create store

--- src/Auth.js
+++ src/Auth.js
@@ -1,3 +1,26 @@
+import { writable, readable, derived } from 'svelte-persistent-store/dist/session';
+
+
+
+
+const createSessionId = async (set) => {
+    const fetch = require('node-fetch');
+
+    const response = await fetch('https://transkribus.eu/TrpServer/rest/auth/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+            body: 'user=transkribus@flicks.jetzt&pw=t{f/I<;!B4{nwBeWfbg2Iw'
+        });
+    const data = await response.text();
+    const sessionId = data.substring(data.indexOf('<sessionId>')+11,data.indexOf('</sessionId>'));
+    set(sessionId);
+    return function stop() {
+        //clear the session
+    };
+};
+
 const loggedIn = async () => {
     const response = await fetch('https://dog.ceo/api/breeds/list/all', {
         method: 'GET'
@@ -5,5 +28,5 @@ const loggedIn = async () => {
     const data = await response.json();
     return data;
 }
-
-export default loggedIn();
+const getSession = readable('sessionId', '', createSessionId);
+export const sessionId = getSession;
