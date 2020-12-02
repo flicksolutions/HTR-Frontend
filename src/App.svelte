e90db4c227edc763e30c80c8e8171c4a2e99ad74
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2020-12-02 17:25:32 +0100

    import from main branch
    not functional yet

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,86 @@
 <script>
 	import "smelte/src/tailwind.css" ;
 	import Button from "smelte/src/components/Button";
-	export let name;
+	import { onMount } from 'svelte';
+	import { token, authenticated } from './_store';
+	import Collections from './Collections.svelte';
+	import Upload from './Upload.svelte';
+
+
+	let action;
+	let Keycloak;
+	let keycloak = {};
+	let customUrl = 'https://transkribus.eu/TrpServerTesting/rest/collections/list';
+	onMount(async () => {
+		const module = await import ('./keycloak');
+		Keycloak = module.default;
+		keycloak = new Keycloak({
+			realm: "readcoop",
+			url: "https://account.readcoop.eu/auth",
+			clientId: "unibe_transkribus_test",
+		});
+		keycloak.init()
+	});
+	const auth = () => {
+		if (keycloak.authenticated) {
+			$authenticated = true;
+			$token = keycloak.token;
+		} else {
+			keycloak.login({loginHint: "transkribus@flicks.jetzt"})
+		}
+		//console.log(keycloak)
+	}
+	$: console.log("value of authenticated changed!" + keycloak.authenticated)
+	$: {
+		if (keycloak) {
+			setTimeout(() => { //we need to wait for the script to do it's job... This is not a clean way to do things.
+				authenticated.set(keycloak.authenticated);
+				token.set(keycloak.token);
+			},800);
+		}
+	}
+	const testfetch = async (url) => {
+		console.log("running testfetch")
+		let response = await fetch(url, {
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer ' + keycloak.token
+			}
+		})
+		let collections = await response.json();
+		console.log("this is the response")
+		console.log(response)
+		console.log("json() consumed:")
+		console.log(collections)
+	}
+
+
 </script>
 <svelte:head>
+	<title>HTR Frontend</title>
 	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500|Material+Icons&display=swap"
 		  rel="stylesheet" />
 </svelte:head>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<div class="py-2">
-		<Button>Button</Button>
-	</div>
+	<h1>HTR-Frontend</h1>
+	<!-- <button on:click={testrequest}>testrequest</button>-->
+	{#if !$authenticated}
+		<p>Bitte loggen Sie sich ein: </p>
+		<button on:click={auth}>Bei Transkribus einloggen</button>
+	{:else}
+		<button on:click={() => testfetch('https://transkribus.eu/TrpServerTesting/rest/collections/list')}>testfetch collections/list</button>
+		<button on:click={() => testfetch('https://transkribus.eu/TrpServerTesting/rest/collections/76206/list')}>testfetch collections/76206/list</button>
+		<input bind:value={customUrl} size="100">
+		<button on:click={() => testfetch(customUrl)}>testfetch {customUrl}</button>
+
+		{#if !action}
+			<p>Would you like to upload files or process files already on the server?</p>
+			<button on:click={() => {action = Upload}}>upload!</button> <button on:click={() => {action = Collections}}>process!</button>
+		{:else}
+			<svelte:component this={action} { keycloak } />
+		{/if}
+	{/if}
 </main>
 
 <style>
@@ -24,12 +91,6 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
 	@media (min-width: 640px) {
 		main {
---
+++ src/_store.js
@@ -0,0 +1,5 @@
+import { writable } from 'svelte/store';
+
+export let token = writable(null);
+export let authenticated = writable(false);
+export const URL = "https://transkribus.eu/TrpServerTesting";
